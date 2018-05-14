0240dabcc880059d94199548855167e1dc0f7e84
Kelly Villalobos <kvdesign1978@gmail.com>
2018-05-14 15:54:48 -0500

    walkthrough answers, realized I didn't understand the exercise, commiting using no suppress warnings

--- src/Author.java
+++ src/Author.java
@@ -10,30 +10,21 @@ public class Author extends Person {
 
     public Author(String firstName, String lastName) {
         super(firstName, lastName);
-        books = new ArrayList();
+        books = new ArrayList<String>();
     }
 
-    /**
-     * @deprecated Use publishedBooks instead
-     */
-    @Deprecated
-    @SuppressWarnings("unchecked")
-    public List<String> getBooks() {
-        return books;
-    }
 
-    @SuppressWarnings("unchecked")
     public List<String> publishedBooks() {
         return books;
     }
 
-    @SuppressWarnings("unchecked")
+
     public void addBook(String book) {
         books.add(book);
     }
 
     @Override
-    public String fullName() {
+    public String sortName() {
         return String.format("%s, %s", lastName, firstName);
     }
 }
